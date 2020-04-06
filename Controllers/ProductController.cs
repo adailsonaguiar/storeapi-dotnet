7432774b77484f942e3caf046ab7072921652daf
Adailson Aguiar Campos Jr <adailsonacj@live.com>
2020-04-06 00:41:06 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using storeapi_dotnet.Data;
+using storeapi_dotnet.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapi_dotnet.Controllers
+{
+    [ApiController]
+    [Route("v1/products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.category).ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            var product = await context.Products.Include(x => x.category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categpries/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products
+                .Include(x => x.category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId == id)
+                .ToListAsync();
+            return products;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using storeapi_dotnet.Models;
+
+namespace storeapi_dotnet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
